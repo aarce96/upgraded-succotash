717ea0c99363532116c66da138e4366e39883fd9
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-06 14:45:13 -0500

    added variables

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// a variable for start time
+let secondsLeft = 70;
+
+let timeEl = document.getElementById("time");
+let hsEl = document.getElementById("highScores");
+let btnsEl = document.getElementById("buttons")
+let vsBtn = document.getElementById("viewScores")
+var questionEl = document.getElementById("questions");
+let resultsEl = document.getElementById("results");
+var choices = document.getElementById("choices");
+let startBtnEl = document.getElementById("startBtn");
