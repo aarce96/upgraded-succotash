f9f004f941d920d2a7b0b1471c6b1d311f06185a
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-06 15:05:28 -0500

    js for time and questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,63 @@
-// a variable for start time
 let secondsLeft = 70;
 
 let timeEl = document.getElementById("time");
 let hsEl = document.getElementById("highScores");
-let btnsEl = document.getElementById("buttons")
-let vsBtn = document.getElementById("viewScores")
-var questionEl = document.getElementById("questions");
+let btnsEl = document.getElementById("buttons");
+let vsBtn = document.getElementById("viewScores");
+let questionEl = document.getElementById("questions");
 let resultsEl = document.getElementById("results");
-var choices = document.getElementById("choices");
+let choicesEl = document.getElementById("choices");
+
 let startBtnEl = document.getElementById("startBtn");
+alert('button pressed!')
+startBtnEl.addEventListener("click", setTimer);
+
+let questionCount = 0;
+let score = 0;
+
+function setTimer() {
+  displayQuestions();
+  let timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "";
+    timeEl.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <= 0 || questionCount === codingQuestions.length) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+function displayQuestions() {
+  removeEls(startBtnEl);
+
+  if (questionCount < questions.length) {
+    questionEl.innerHTML = codingQuestions[questionCount].title;
+    choicesEl.textContent = "";
+
+    for (let i = 0; i < codingQuestions[questionCount].answers.length; i++) {
+      let el = document.createElement("button");
+      el.innerText = codingQuestions[questionCount].answers[i];
+      el.setAttribute("data-id", i);
+      el.addEventListener("click", function (event) {
+        event.stopPropagation();
+
+        if (el.innerText === codingQuestions[questionCount].answer) {
+          score += secondsLeft;
+        } else {
+          score -= 10;
+          secondsLeft = secondsLeft - 10;
+        }
+
+        questionEl.innerHTML = "";
+
+        if (questionCount === codingQuestions.length) {
+          return;
+        } else {
+          questionCount++;
+          displayQuestions();
+        }
+      });
+      choicesEl.append(el);
+    }
+  }
+}
