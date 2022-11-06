317cadde25b9f88ed56de22bbbe28ec78c408800
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-06 14:33:56 -0500

    added coding questions

---
+++ assets/js/codingQuestions.js
@@ -0,0 +1,31 @@
+var codingQuestions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    correctAnswer: "3. Alerts",
+  },
+
+  {
+    question: "The condition in an if/else statement is enclosed with _____.",
+    answers: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
+    correctAnswer: "3. Parenthesis",
+  },
+
+  {
+    question: "Arrays in JavaScript can be used to store _____.",
+    answers: ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"],
+    correctAnswer: "4. All of the Above",
+  },
+
+  {
+    question: "String values must be enclosed within _____when being assigned to variables.",
+    answers: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"],
+    correctAnswer: "3. Quotes",
+  },
+
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. console.log"],
+    correctAnswer: "4. console.log",
+  },
+];
